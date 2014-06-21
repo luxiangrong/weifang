a69cca500ad7eeea02d0ed12b043689646a9ff6c
LuXiangrong <luxiangrongwave@gmail.com>
2014-06-21 17:30:13 +0800

    添加swiper

--- script/function.js
+++ script/function.js
@@ -2,11 +2,19 @@
 (function($) {
 	$(function() {
 		$(document).ready(function() {
-			$(".welcome").on('click touchstart', function(e){
+			$(".welcome").on('click touchstart', function(e) {
 				e.preventDefault();
 				$this = $(this);
 				$this.addClass('slideUp');
 				$(".welcome").off('click touchend');
+			});
+
+			var swiper = new Swiper('.swiper-container', {
+				pagination : '.pagination',
+				loop : true,
+				grabCursor : true,
+				paginationClickable : true,
+				calculateHeight: true
 			});
 		});
 	});
