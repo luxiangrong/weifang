989f2d0a8455a834fe4746a5c3f4d716e55134d4
luxiangrong <luxiangrongwave@gmail.com>
2014-06-21 22:53:57 +0800

    首页完成

--- script/function.js
+++ script/function.js
@@ -14,8 +14,28 @@
 				loop : true,
 				grabCursor : true,
 				paginationClickable : true,
-				calculateHeight: true
+				calculateHeight: true,
+				roundLengths: true,
 			});
+			var winWidth = $(window).width();
+			var slideImgWidth = $("#slides img").width();
+			var slideImgHeight = $("#slides img").height();
+			
+			
+			console.log(slideImgWidth + ":" + slideImgHeight);
+			
+			$("#slides").slidesjs({
+		        width: 580,
+		        height: 326,
+		        navigation: {
+		        	active: false	
+		        },
+		        pagination: {
+			      active: false,
+			      effect: "fade"
+			        // [string] Can be either "slide" or "fade".
+			    }
+		    });
 		});
 	});
 })(jQuery);
